80e679972c1e94d37aae13a29118c9df84239ce1
BenjaminPondal <benjaminpondal@hotmail.com>
2025-08-29 18:27:01 -0300

    Add ejercicio 11

--- Guia1/Ejercicio11/ej11.c
+++ Guia1/Ejercicio11/ej11.c
@@ -6,18 +6,24 @@ int main()
 #define N 4
     int16_t array[] = {1, 2, 3, 4};
 
+    size_t rotaciones;
+    scanf("%zx", &rotaciones);
+
     if (N < 2)
         return 1;
 
-    int16_t first = array[0];
-
-    for (size_t i = 1; i < N; i++)
+    for (size_t i = 0; i < rotaciones; i++)
     {
-        int16_t temp = array[i];
-        array[i - 1] = temp;
-    }
+        int16_t first = array[0];
 
-    array[N - 1] = first;
+        for (size_t i = 1; i < N; i++)
+        {
+            int16_t temp = array[i];
+            array[i - 1] = temp;
+        }
+
+        array[N - 1] = first;
+    }
 
     for (size_t i = 0; i < N; i++)
     {
--- Guia1/Ejercicio11/ej11.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-void printArray(int16_t array[4]);
